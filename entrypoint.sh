be9a0188792a7eaab484a3f5323de3036da0fbab
onlymore <rocheralex56@gmail.com>
2024-03-06 17:02:02 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,8 @@ ls ./site
 # Directory containing the built mkdocs site
 MKDOCS_BUILD_DIR='./site'
 
-# Temporary file paths
-TEMP_SSH_PRIVATE_KEY_FILE='../private_key.pem'
 RSYNC_OPTIONS="-avz --delete"
+
 
 # Remote path cannot be empty
 if [ -z "$6" ]; then
@@ -17,27 +16,10 @@ if [ -z "$6" ]; then
    exit 1
 fi
 
-echo 'Deploying using rsync with SSH key authentication...'
+echo 'Deploying using rsync...'
 
-# Check if password is provided
-if [ -z != $10 ]; then
-	echo 'Use sshpass'
-	apk add sshpass
-
-	# Ensure the private key file content is stored
-	printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
-	chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
-
-	# Sync mkdocs site to remote server using sshpass and rsync
-	sshpass -p $10 rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE" $MKDOCS_BUILD_DIR/ $1@$2:$6
-else
-	# Ensure the private key file content is stored
-	printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
-	chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
-
-	# Sync mkdocs site to remote server
-	rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE" $MKDOCS_BUILD_DIR/ $1@$2:$6
-fi
+# Sync site to remote server using sshpass and rsync
+sshpass -p $10 rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3" $MKDOCS_BUILD_DIR/ $1@$2:$6
 
 echo 'Deploy Success'
 
