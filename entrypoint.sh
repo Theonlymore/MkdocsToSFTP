3bdeddaadbbea8adad7e94d31233c01e78ad9eec
onlymore <rocheralex56@gmail.com>
2024-03-06 16:58:10 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-
 # Build mkdocs site.
 echo "Building mkdocs site..."
 mkdocs build
@@ -20,12 +19,25 @@ fi
 
 echo 'Deploying using rsync with SSH key authentication...'
 
-# Ensure the private key file content is stored
-printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
-chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
+# Check if password is provided
+if [ -z != $10 ]; then
+	echo 'Use sshpass'
+	apk add sshpass
 
-# Sync mkdocs site to remote server
-rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE" $MKDOCS_BUILD_DIR/ $1@$2:$6
+	# Ensure the private key file content is stored
+	printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
+	chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
+
+	# Sync mkdocs site to remote server using sshpass and rsync
+	sshpass -p $10 rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE" $MKDOCS_BUILD_DIR/ $1@$2:$6
+else
+	# Ensure the private key file content is stored
+	printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
+	chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
+
+	# Sync mkdocs site to remote server
+	rsync $RSYNC_OPTIONS -e "ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE" $MKDOCS_BUILD_DIR/ $1@$2:$6
+fi
 
 echo 'Deploy Success'
 
